e1f8ed12f06dff5f52b1fdbc7d89da59c23bd5f7
Dima Chijov <chijovdmitriy@gmail.com>
2019-02-02 19:54:18 +0300

    Build system (v0.1)
    early build system (v0.1)
    -Object can follow to finger
    -Object material can to changes

---
+++ [RTS]Village in the sky/Assets/Code/LocalChangeMaterial.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+
+namespace BuildSpace
+{
+    public class LocalChangeMaterial : MonoBehaviour
+    {
+        private Material material;
+        private bool LoopStop { get; set; }
+
+        private void Start()
+        {
+            material = Resources.Load("ThatchRoof", typeof(Material)) as Material;
+            LoopStop = true;
+        }
+
+        void Update()
+        {
+            if (Builder.StopFlag && LoopStop)
+            {
+                gameObject.GetComponent<MeshRenderer>().material = material;
+                //Builder.ChangeFlag = false;
+                Debug.Log(Builder.StopFlag);
+                LoopStop = false;
+                //enabled = false;
+            }
+        }
+    }
+}
