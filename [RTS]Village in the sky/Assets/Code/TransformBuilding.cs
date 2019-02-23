646d3398ae97ccc8218f3a7d5b1f7606c6bc4d6f
Dima Chijov <chijovdmitriy@gmail.com>
2019-02-23 18:09:52 +0300

    Intermediate Save v0.2
    Кажется я немного пофиксил перемещение, но я не уверен.

--- [RTS]Village in the sky/Assets/Code/TransformBuilding.cs
+++ [RTS]Village in the sky/Assets/Code/TransformBuilding.cs
@@ -9,7 +9,6 @@ namespace BuildSpace
     {
         private Ray ray;
         private RaycastHit hit;
-        private RaycastHit hitBottom;
         private Touch touch;
         // private Input touch;
         //Всю эту залупу нужно начисто переписывать, говнокод
@@ -18,18 +17,9 @@ namespace BuildSpace
 
         //Переписать скрипт без всяких трансформ позишон и прочьего говна
         //Всю эту залупу нужно начисто переписывать, говнокод
-
-
-
-        private bool firstTouchFlag;
-        private bool stopReadingTouch;
-        private Vector3 position;
-
-        private Vector3 shiftPositionVector;
-        private Vector3 secondFingerPosition;
-        private Vector3 firstFingerPosition;
-
-
+        private Vector3 hitPosition;
+        private Vector3 oldPosition;
+        private bool firsTouchFlag;
         private bool checkPC;
 
         private void Start()
@@ -73,42 +63,78 @@ namespace BuildSpace
 
         private void Update()
         {
-            if (Input.GetMouseButtonDown(0)) checkPC = true;
-            if (Input.GetMouseButtonUp(0)) checkPC = false;
-
-            if (!checkPC) return;
-
-            ray = Camera.main.ScreenPointToRay(Input.mousePosition);
-
-            if(Physics.Raycast(ray, out hit))
             {
-                if (hit.collider.name != gameObject.name) // Это что за залупа ? Нужно понять, как работает и нахуя здесь эта проверка
+                if (Input.GetMouseButtonDown(0)) checkPC = true;
+                if (Input.GetMouseButtonUp(0))
                 {
-                    position = gameObject.transform.position;
-                    firstTouchFlag = false;
-                    return;
+                    checkPC = false;
+                    firsTouchFlag = false;
                 }
+                if (!checkPC) return;
             }
 
-            if (Physics.Raycast(position + transform.up, -Vector3.up, out hitBottom))
+            if (!Physics.Raycast(gameObject.transform.position + transform.up, -Vector3.up))
             {
-                if (hitBottom.collider.name != "Island") return;
-
-                if (!firstTouchFlag)
-                {
-                    firstFingerPosition = hit.point;
-                    firstTouchFlag = true;
-                    position = gameObject.transform.position;
-                    return;
-                }
+               // gameObject.transform.position -= new Vector3(0.01f * hit.point.x, 0f , 0.01f * hit.point.z);
+                return;
             }
 
-            secondFingerPosition = hit.point;
-            shiftPositionVector = secondFingerPosition - firstFingerPosition;
-            shiftPositionVector = new Vector3(shiftPositionVector.x, 0f, shiftPositionVector.z);
-            gameObject.transform.position += shiftPositionVector;
-            firstFingerPosition = hit.point;
+
+            {
+                ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+                if (Physics.Raycast(ray, out hit))
+                {
+                    if(hit.collider.name == gameObject.name)
+                    {
+                        if (!firsTouchFlag)
+                        {
+                            hitPosition = hit.point;
+                            firsTouchFlag = true;
+                        }
+                        else
+                        {
+                            gameObject.transform.position = new Vector3(hit.point.x + gameObject.transform.position.x - hitPosition.x, 0.5f, hit.point.z + gameObject.transform.position.z - hitPosition.z);
+                            hitPosition = hit.point;
+                        }
+                    }
+                }
+                oldPosition = gameObject.transform.position;
+            }
+
+
+
+            //if(Physics.Raycast(ray, out hit))
+            //{
+            //    if (hit.collider.name != gameObject.name) // Это что за залупа ? Нужно понять, как работает и нахуя здесь эта проверка
+            //    {
+            //        Debug.Log("1");
+            //        position = gameObject.transform.localPosition;
+            //        firstTouchFlag = false;
+            //        return;
+            //    }
+            //}
+
+            //if (Physics.Raycast(position + transform.up, -Vector3.up, out hitBottom))
+            //{
+            //    Debug.Log(hitBottom.collider.name);
+            //    if (hitBottom.collider.name != "Island") Debug.Log("3");
+
+            //    if (!firstTouchFlag)
+            //    {
+            //        firstFingerPosition = hit.point;
+            //        firstTouchFlag = true;
+            //        position = gameObject.transform.localPosition;
+            //        return;
+            //    }
+            //}
+
+            //secondFingerPosition = hit.point;
+            //shiftPositionVector = secondFingerPosition - firstFingerPosition;
+            //shiftPositionVector = new Vector3(shiftPositionVector.x, 0f, shiftPositionVector.z);
+            //gameObject.transform.position += shiftPositionVector;
+            //firstFingerPosition = hit.point;
         } // VERSION FOR PC INSERT
+
     }
 
 }
