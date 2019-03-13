23dc4521bbd84ed902cadebbf7c02a9dcbb9ef5d
Dima Chijov <chijovdmitriy@gmail.com>
2019-03-13 20:07:43 +0300

    Loca lSave 3

--- [RTS]Village in the sky/Assets/Code/CameraMove.cs
+++ [RTS]Village in the sky/Assets/Code/CameraMove.cs
@@ -11,6 +11,8 @@ public class CameraMove : MonoBehaviour
 
     private float rotation;
 
+    private float rad;
+
     private float xRotation;
     private float zRotation;
 
@@ -68,16 +70,23 @@ public class CameraMove : MonoBehaviour
             zDeltaPosition = zPosition - Input.mousePosition.y;
             xDeltaPosition = xPosition - Input.mousePosition.x;
 
-            Debug.Log(zDeltaPosition + "///" + xDeltaPosition);
+            //Debug.Log(zDeltaPosition + "///" + xDeltaPosition);
 
             zLocalPosition = zDeltaPosition;
             xLocalPosition = xDeltaPosition;
 
+            rad = rotation * 3.1415f;
+            rad = rad / 180;
+
 
 
             //zDeltaPosition *=0.05f;
-            zDeltaPosition = ((zLocalPosition * Mathf.Cos(rotation)) - (xLocalPosition * Mathf.Sin(rotation))) * Time.deltaTime;
-            xDeltaPosition = ((xLocalPosition * Mathf.Cos(rotation)) + (zLocalPosition * Mathf.Sin(rotation))) * Time.deltaTime;
+            zDeltaPosition = ((zLocalPosition * Mathf.Cos(rad)) - (xLocalPosition * Mathf.Sin(rad))) * Time.deltaTime;
+            // Debug.Log("rotation" + rad + "sin" + Mathf.Sin(rad));
+            Debug.Log(rad);
+            xDeltaPosition = ((xLocalPosition * Mathf.Cos(rad)) + (zLocalPosition * Mathf.Sin(rad))) * Time.deltaTime;
+
+            //Debug.Log(zDeltaPosition + "|" + xDeltaPosition);
             //zDeltaPosition = ((xDeltaPosition * Mathf.Cos(rotation))) * Time.deltaTime;
             //xDeltaPosition = ((zDeltaPosition * Mathf.Cos(rotation)) + (xDeltaPosition * Mathf.Sin(rotation))) * Time.deltaTime;
             //xDeltaPosition = ((zDeltaPosition * Mathf.Cos(rotation))) * Time.deltaTime;
@@ -88,7 +97,7 @@ public class CameraMove : MonoBehaviour
 
             //deltaVector = currentPosition - (Vector2)Input.mousePosition;
             //Debug.Log(deltaVector);
-            cam.transform.position = new Vector3(cam.transform.position.x + xDeltaPosition, cam.transform.position.y, cam.transform.position.z + zDeltaPosition);
+            cam.transform.localPosition = new Vector3(cam.transform.localPosition.x + xDeltaPosition, cam.transform.localPosition.y, cam.transform.localPosition.z + zDeltaPosition);
             //currentPosition = Input.mousePosition;
             zPosition = Input.mousePosition.y;
             xPosition = Input.mousePosition.x;
@@ -99,8 +108,8 @@ public class CameraMove : MonoBehaviour
 
     public void Rotate(bool Side) // true - rotate to right // false - rotate to left
     {
-        if (Side)rotation += 30f;
-        if (!Side)rotation -= 30f;
+        if (Side)rotation += 90f;
+        if (!Side)rotation -= 90f;
         if (rotation > 360) rotation -= 360;
         if (rotation < 0) rotation += 360;
         //Debug.Log(rotation);
